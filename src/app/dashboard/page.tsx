5a77e611de70ea8330a72145b0581d13d8cf849c
Soham <gsoham101@gmail.com>
2025-08-02 19:33:22 +0530

    started dashboard content

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,11 @@
+import { DashboardPage } from "@/components/dashboard-page"
 import { db } from "@/db"
 import { currentUser } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation"
+import { DashboardPageContent } from "./dashboard-page-content"
+import { CreateEventCategoryModal } from "@/components/create-event-category-modal"
+import { Button } from "@/components/ui/button"
+import { PlusIcon } from "lucide-react"
 
 const Page = async () => {
 
@@ -19,7 +24,14 @@ const Page = async () => {
     }
 
     return (
-        <p>Dashboard</p>
+        <DashboardPage cta={<CreateEventCategoryModal>
+            <Button>
+                <PlusIcon className="size-4 mr-2"/>
+                Add Category
+            </Button>
+        </CreateEventCategoryModal>} title="Dashboard">
+            <DashboardPageContent />
+        </DashboardPage>
     )
 }
 
--- src/server/procedures.ts
+++ src/server/procedures.ts
@@ -1,43 +1,36 @@
-import { Pool } from "@neondatabase/serverless"
-import { PrismaNeon } from "@prisma/adapter-neon"
-import { PrismaClient } from "@prisma/client"
-import { Redis } from "@upstash/redis/cloudflare"
-import { env } from "hono/adapter"
-import { cacheExtension } from "./__internals/db/cache-extension"
+import { currentUser } from "@clerk/nextjs/server"
 import { j } from "./__internals/j"
+import { db } from "@/db"
+import { HTTPException } from "hono/http-exception"
 
-/**
- * Middleware for providing a built-in cache with your Prisma database.
- *
- * You can remove this if you don't like it, but caching can massively speed up your database queries.
- */
+ const authMiddleware = j.middleware(async({c, next}) => {
+  const authHeader = c.req.header("Authorization")
+  if(authHeader) {
+    // Bearer <API_KEY>
+    const apiKey = authHeader.split(" ")[1]
+    const user = await db.user.findUnique({
+      where: {apiKey},
+    })
+    if(user) return next({user})
+  }
 
-const extendedDatabaseMiddleware = j.middleware(async ({ c, next }) => {
-  const variables = env(c)
+  const auth = await currentUser()
 
-  const pool = new Pool({
-    connectionString: variables.DATABASE_URL,
+  if(!auth){
+    throw new HTTPException(401, {message: "Unauthorized"})
+  }
+
+  const user = await db.user.findUnique({
+    where: {externalId: auth.id}
   })
 
-  const adapter = new PrismaNeon(pool)
+  if(!user){
+    throw new HTTPException(401, {message: "Unauthorized"})
+  }
 
-  const redis = new Redis({
-    token: variables.REDIS_TOKEN,
-    url: variables.REDIS_URL,
-  })
-
-  const db = new PrismaClient({
-    adapter,
-  }).$extends(cacheExtension({ redis }))
-
-  // Whatever you put inside of `next` is accessible to all following middlewares
-  return await next({ db })
-})
-
- const authMiddleware = j.middleware(({next}) => {
-  const user = {name: "John"}
   return next({user})
  })
+
 
 /**
  * Public (unauthenticated) procedures
@@ -45,5 +38,5 @@ const extendedDatabaseMiddleware = j.middleware(async ({ c, next }) => {
  * This is the base piece you use to build new queries and mutations on your API.
  */
 export const baseProcedure = j.procedure
-export const publicProcedure = baseProcedure.use(extendedDatabaseMiddleware)
+export const publicProcedure = baseProcedure
 export const privateProcedure = publicProcedure.use(authMiddleware)
