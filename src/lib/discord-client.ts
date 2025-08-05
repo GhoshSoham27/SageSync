189332a5fbe3845f1366b4051b3328965df26fd4
Soham <gsoham101@gmail.com>
2025-08-05 23:51:08 +0530

    started discord backend

---
+++ src/lib/discord-client.ts
@@ -0,0 +1,9 @@
+import { REST } from "@discordjs/rest"
+
+export class DiscordClient {
+    private rest: REST
+
+    constructor(token: string | undefined) {
+        this.rest = new REST
+    }
+}
